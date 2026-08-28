404a9fb153040cc8d01ca99de57c4b7c58951243
yaks <1872767132@qq.com>
2026-08-28 14:32:29 +0800

    Python whilr循环练习1

--- hello.py
+++ hello.py
@@ -1,19 +1,14 @@
-#BMI=体重/（身高**2）
-user_weight=float(input("请输入您的体重（单位：kg）:"))
-user_height=float(input("请输入您的身高（单位：m）:"))
-user_BMI=user_weight/(user_height)**2
-print("您的BMI值5为："+str(user_BMI))
-
-# 偏瘦：user_MBI<=18.5
-# 正常：18.5<user_BMI<=25
-# 偏胖：25<user_BMI<=30
-# 肥胖：user_BMI>30
-
-if user_BMI<=18.5:
-    print("此BMI值属于偏瘦范围。")
-elif 18.5<=user_BMI<=25:
-    print("此BMI值属于正常范围。")
-elif 25<=user_BMI<=30:
-    print("此BMI值属于偏胖范围。")
+print("哈喽呀！我是一个求平均值的")
+total = 0
+count = 0
+user_input = input("请输入数字（完成所有数字输入后，请输入q终止程序）")
+while user_input != "q":
+    num = float(user_input)
+    total += num
+    count += 1
+    user_input = input("请输入数字（完成所有数字输入后，请输入q终止程序）")
+if count == 0:
+    result = 0
 else:
-    print("此BMI值属于肥胖范围。")
+    result = total / count
+print("您输入的数字的平均值为"+str(result))
